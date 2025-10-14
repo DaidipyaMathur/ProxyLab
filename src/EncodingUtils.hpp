ac6d303bdb0c03ae1d10305676ee334471c45a27
Sakura1711 <aggitanjali2005@gmail.com>
2025-10-14 18:18:39 +0530

    Update EncodingUtils.hpp

--- src/EncodingUtils.hpp
+++ src/EncodingUtils.hpp
@@ -3,7 +3,17 @@
 
 #include <string>
 
-// Decompresses a gzip-encoded string
-std::string gzip_decompress(const std::string& compressed_data);
+std::string base64_encode(const std::string&);
+std::string base64_decode(const std::string&);
+
+std::string url_encode(const std::string&);
+std::string url_decode(const std::string&);
+
+std::string hex_encode(const std::string&);
+std::string hex_decode(const std::string&);
+
+std::string gzip_decompress(const std::string&);
+std::string gzip_compress(const std::string&, int level = Z_BEST_COMPRESSION);
+
 
 #endif // ENCODINGUTILS_HPP
